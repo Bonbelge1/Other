d6fb81a4cb2a254aaabac7c6d9a2297ba860e736
Nico <nicolasparadis42@gmail.com>
2016-11-06 17:34:04 -0500

    add classes to evolution.py

--- evolution.py
+++ evolution.py
@@ -1,31 +1,60 @@
 import pygame
+import random
+import time
 from pygame.locals import *
 
-def input(events):
-    ''' Function to handle key events and quit '''
-    for event in events:
-        if event.type == QUIT:
-            quit()
-        else:
-        	if event.type == KEYDOWN:
-                 print(event.key)
+# Class that define genetic
+class Being:
+
+	beingCreated = 0
+
+	def __init__(self):
+		self.serialNb = Being.beingCreated 
+		self.name = 'default_name'
+		self.weight = 20
+		self.pos = (50, 80)
+		self.reproLimit = 30
+		self.vect = (random.randrange(10), random.randrange(10)) 
+		Being.beingCreated += 1
+
+# class for ground tiles
+class Tiles:
+	def __init__(self):
+		self.type = "grass"
+		self.food = 20
+		self.growing = 1
 
 
-pygame.init()
-
-window = pygame.display.set_mode((640, 480))
-pygame.display.set_caption("Pygame Test")
-
-DISPLAYSURF = pygame.display.set_mode()
-pygame.draw.circle(DISPLAYSURF, (255, 255, 255), (150, 250), 30)
-
-
-
-pygame.display.flip()
-
-
-booleanCondition = 1
-while booleanCondition:
-   input(pygame.event.get())
+# Function for self-reproduction
+def repro(listBeing):
+	for i in range(len(listBeing)):
+		print(listBeing[i].weight)
    
-   
+ def main():
+	
+ 	# Game clock
+	pygame.time.delay(100) 
+	
+	# Creation of the first generation
+	gen = [Being() for i in range(10)]
+	print(repro(gen))
+
+	# Creation of the window
+	pygame.init()
+	window = pygame.display.set_mode((640, 480))
+	pygame.display.set_caption("Pygame Test")
+
+	# Test for printing a circle
+	DISPLAYSURF = pygame.display.set_mode()
+	pygame.draw.circle(DISPLAYSURF, (255, 255, 255), (150, 250), 30)
+
+	# Update the window
+	pygame.display.flip()
+
+	# Infinite loop
+	booleanCondition = 1
+	while booleanCondition:
+
+
+if __name__ == '__main__':
+	main() 
