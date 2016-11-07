4d50eb2b622e9765d0fa75bda018fdf94e41651e
Bonbelge1 <nicolasparadis42@gmail.com>
2016-11-07 10:58:28 -0500

    Update evolution.py

--- evolution.py
+++ evolution.py
@@ -11,20 +11,25 @@ class Being:
 	def __init__(self):
 		self.serialNb = Being.beingCreated 
 		self.name = 'default_name'
+		self.color = 'BLUE'
 		self.weight = 20
 		self.pos = (50, 80)
 		self.reproLimit = 30
 		self.vect = (random.randrange(10), random.randrange(10)) 
 		Being.beingCreated += 1
 
-# class for ground tiles
+# Class for ground tiles
 class Tiles:
 	def __init__(self):
 		self.type = "grass"
 		self.food = 20
 		self.growing = 1
 
-
+# Window information
+info = pygame.display.Info()
+sw = info.current_w
+sh = info.current_h
+		
 # Function for self-reproduction
 def repro(listBeing):
 	for i in range(len(listBeing)):
@@ -35,25 +40,40 @@ def repro(listBeing):
  	# Game clock
 	pygame.time.delay(100) 
 	
+	# Colors
+	BLACK = (0, 0, 0)
+	WHITE = (255, 255, 255)
+	RED = (255, 0, 0)
+	GREEN = (0, 255, 0)
+	BLUE = (0, 0, 255)
+	
 	# Creation of the first generation
 	gen = [Being() for i in range(10)]
-	print(repro(gen))
+	#print(repro(gen))
 
 	# Creation of the window
 	pygame.init()
 	window = pygame.display.set_mode((640, 480))
+	windowSurface.fill(WHITE)
 	pygame.display.set_caption("Pygame Test")
 
-	# Test for printing a circle
+	# Surface
 	DISPLAYSURF = pygame.display.set_mode()
-	pygame.draw.circle(DISPLAYSURF, (255, 255, 255), (150, 250), 30)
-
-	# Update the window
-	pygame.display.flip()
 
 	# Infinite loop
-	booleanCondition = 1
-	while booleanCondition:
+	while True:
+		windowSurface.fill(WHITE)
+		for i in range(len(gen)):
+			pygame.draw.circle(DISPLAYSURF, BLUE, gen[i].pos, gen[i].weight)
+			gen[i].pos = ((gen[i].pos[0] + gen[i].vect[0]) % sh, (gen[i].pos[1] + gen[i].vect[0]) % sw)
+		
+		# Update the window
+		pygame.display.flip()
+		
+		for event in pygame.event.get():
+        		if event.type == QUIT:
+            			pygame.quit()
+           			sys.exit()
 
 
 if __name__ == '__main__':
